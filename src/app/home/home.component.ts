695cd35a05f926df917c32f256a3eaa4f2fd4693
enosgb <enosgb55@gmail.com>
2022-06-28 15:40:08 -0300

    ng select

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  selectedCar: number;
+
+  cars = [
+      { id: 1, name: 'Volvo' },
+      { id: 2, name: 'Saab' },
+      { id: 3, name: 'Opel' },
+      { id: 4, name: 'Audi' },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
